a4c13a3425427e084fb5a0d0f277587daafa5f12
bion-slmn <bionsol25@gmail.com>
2024-05-24 17:39:47 +0300

    I have added the option one to one messaging in the server

--- client.py
+++ client.py
@@ -1,41 +1,60 @@
 import socket
 import json
 import threading
+import logging
 
 
 class Client:
-    '''
-    start Client
-    '''
     def __init__(self, port, ip_address):
-        '''
-        initialise the server
-        '''
         self.port = port
         self.ip_address = ip_address
         self.server = ''
 
     def create_server(self):
-        '''
-        create a server socket
-        '''
         try:
+            # connect to the server
             self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.server.connect((self.ip_address, self.port))
             self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            print(f' Starting server at PORT {self.port} IP {self.ip_address}')
+            self.message_logger(
+                f'Starting server at PORT {self.port} IP {self.ip_address}')
         except Exception as e:
-            print(f'Error occured: {e}')
+            self.message_logger(f'Error occurred: {e}')
 
-    
+    def handle_client(self):
+        while True:
+            try:
+                msg = self.server.recv(1024).decode('utf-8')
+                if not msg:
+                    self.server.close()
+                self.message_logger(msg)
+                print()
+            except Exception as error:
+                self.message_logger(f'Error: {error}', 30)
+                self.server.close()
+                break
+
     def start_client(self):
-        '''
-        start the client
-        '''
+        phone_no = input('Enter Your Phone No: ')
+        send_to = input('Enter the phone number :')
+
         self.create_server()
-        data = {'from': '701036054'}
-        json_data = json.dumps(data)
-        self.server.send(json_data.encode('utf-8'))
+
+        # Start the client handler in a separate thread
+        client_handler_thread = threading.Thread(target=self.handle_client)
+        client_handler_thread.start()
+
+        while True:
+            msg = input('Enter message :')
+            data = {'from': phone_no, 'text': msg, 'to': send_to}
+            json_data = json.dumps(data)
+            self.server.send(json_data.encode('utf-8'))
+
+    @staticmethod
+    def message_logger(message, level=20):
+        logging.basicConfig(level=logging.INFO,
+                            format="%(asctime)s %(levelname)s %(message)s")
+        logging.log(level, message)
 
 
 client = Client(8000, 'localhost')
--- server.py
+++ server.py
@@ -5,12 +5,14 @@ Create a server to chat app
 import socket
 import json
 import threading
+import logging
 
 
 class Server:
     '''
     Defines a server object for the chat app
     '''
+
     def __init__(self, port, ip_address):
         '''
         initialise the server
@@ -26,10 +28,13 @@ class Server:
         '''
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((self.ip_address, self.port))
+
+        # make the port reusable
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.listen(5)
-        print(f' Starting server at PORT {self.port} IP {self.ip_address}')
-    
+        self.message_logger(
+            f' Starting server at PORT {self.port} IP {self.ip_address}')
+
     def accept_connection(self):
         '''
         accept connection from the client
@@ -40,36 +45,81 @@ class Server:
         '''
         for each clieent,  create a thread and handle the conmunication
         '''
-        thread = threading.Thread(target=self.handle_client, args=(client_socket, ip_address))
+        thread = threading.Thread(
+            target=self.handle_client, args=(
+                client_socket, ip_address))
         thread.start()
+
+    def send_message(self, message):
+        '''
+        send a message to the receiver number
+        '''
+        try:
+            reciever_number = message.get('to')
+            receiver_socket = self.clients.get(reciever_number)
+
+            if receiver_socket:
+                data = {'from': reciever_number,
+                        'message': message.get('text'),
+                        }
+                json_data = json.dumps(data)
+                receiver_socket.send(json_data.encode('utf-8'))
+                return True
+            return False
+        except Exception as error:
+            del self.clients[reciever_number]
+            message_logger(f'Error : {reciever_number} Not connected')
 
     def handle_client(self, client_socket, address):
         '''
         handle iformation from the client
         '''
-        message = client_socket.recv(1024).decode('utf-8')
-        message = json.loads(message)
-        print(f'Recieved message : { message}')
 
+        while True:
+            message = client_socket.recv(1024).decode('utf-8')
+
+            if not message:
+                client_socket.close()
+                break
+
+            message = json.loads(message)
+
+            # store the client
+            client_number = message.get('from')
+            self.clients[client_number] = client_socket
+            sent = self.send_message(message)
+
+            if not sent:
+                client_socket.send(b'Phone number your sennt to no registered')
 
     def start_server(self):
         '''
-        start the server 
-        ''' 
+        start the server
+        '''
         self.create_server()
         connected = True
 
         while connected:
-            conn, address = self.accept_connection()
-            self.start_thread(conn, *address)
+            try:
+                # connect accept connection
+                conn, address = self.accept_connection()
+                self.message_logger(f'Connection from {address}')
+
+                # start a thread for each connection
+                self.start_thread(conn, *address)
+
+            except Exception as error:
+                self.message_logger('Client Error, clossed connection', 30)
+                conn.close()
+
+    @staticmethod
+    def message_logger(message, level=20):
+        logging.basicConfig(level=logging.INFO,
+                            format="%(asctime)s %(levelname)s %(message)s")
+        logging.log(level, message)
 
 
-
-server = Server(8000, 'localhost')
-server.start_server()
-
-    
-
-
-
-    
+if __name__ == '__main__':
+    server = Server(8000, 'localhost')
+    server.message_logger
+    server.start_server()
